52acd303b2f55c0316a3ae1bf765c8e04d18a5de
vsiago <101620032+vsiago@users.noreply.github.com>
2022-12-15 00:21:56 +0000

    commit pelo celular

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import styles from '../styles/Globals.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início do App</title>
+      </Head>
       <h1>Início do Aplicativo</h1>
     </div>
   )
